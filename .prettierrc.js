1ee90b867f12c32a85b586676b8def85bfadad1b
HFourmy <nice_fourmy@hotmail.com>
2020-11-02 09:31:25 +0100

    Added typescript / prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	singleQuote: false,
+	trailingComma: "all",
+	useTabs: true,
+	semi: true,
+	endOfLine: "auto",
 };
